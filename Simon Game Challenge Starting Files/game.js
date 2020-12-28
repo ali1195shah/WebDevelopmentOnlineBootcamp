a6ca8d415d70634c7a1523a2b48fe68da091475b
Ali <ali1195shah@gmail.com>
2020-12-28 17:55:34 -0500

    u

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,78 @@
+let gamePattern = []
+let userClickedPattern = []
+let level = 0
+let buttonColours = ["red", "blue", "green", "yellow"]
+let started = false;
+
+$(document).keydown(function() {
+    if (!started) {
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
+  });
+
+  $(".btn").on("click", function(){
+    let userChosenColour = $(this).attr("id")
+    userClickedPattern.push(userChosenColour)
+
+    playSound(userChosenColour)
+    animatePress(userChosenColour)
+
+    checkAnswer(userClickedPattern.length-1)
+})
+
+function nextSequence() {
+    userClickedPattern = [];
+    level++
+    $("h1").text("Level " + level)
+
+    let randomNumber = Math.floor(Math.random() * 4) 
+    let randomChosenColour = buttonColours[randomNumber]
+
+    gamePattern.push(randomChosenColour)
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour)
+}
+
+function playSound(name){
+    let audio = new Audio("./sounds/" + name + ".mp3")
+    audio.play()
+}
+
+function animatePress(currentColor){
+    $("#" + currentColor).addClass("pressed")
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+      }, 100);
+}
+
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        if (userClickedPattern.length === gamePattern.length){
+            setTimeout(function () {
+              nextSequence()
+            }, 1000)
+          }
+    }
+    else
+    {
+        let audio = new Audio("./sounds/wrong.mp3")
+        audio.play()
+        $("body").addClass("game-over")
+        setTimeout(function () {
+            $("body").removeClass("game-over")
+        }, 200)
+
+          $("#level-title").text("Game Over, Press Any Key to Restart");
+
+          startOver()
+    }
+}
+
+function startOver(){
+    level = 0
+    gamePattern = []
+    started = false
+}
---
+++ intro-to-node/index.js
@@ -0,0 +1,7 @@
+let supervillains = require("supervillains");
+let superheroes = require("superheroes");
+
+let supervillainsName = supervillains.random();
+let superheroesName = superheroes.random();
+
+console.log(superheroesName + " vs. " + supervillainsName);
