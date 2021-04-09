dc12ecc7aedf97eada93554820cec66055f6c1a3
Mehsum Shah <ali1195shah@gmail.com>
2021-04-09 14:20:13 -0400

    hero

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
 
 const app = express();
 
@@ -11,8 +12,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public")); // makes css visiable in the browser
 
 app.get('/', function(req, res){
-    let today = new Date();
-    day = "";
+    // let today = new Date();
+    // day = "";
 
     // if(today.getDay() === 0 || today.getDay === 6){
     //     day = 'Weekend';
@@ -20,34 +21,34 @@ app.get('/', function(req, res){
     //     day = 'Weekday';
     // }
 
-    switch (today.getDay()) {
-        case 0:
-            day = 'Sunday'
-            break;
-        case 1: 
-            day = 'Monday'
-            break;
-        case 2: 
-            day = 'Tuesday'
-            break;
-        case 3:
-            day = 'Wednesday'
-            break;
-        case 4:
-            day = 'Thursday'
-            break;
-        case 5:
-            day = 'Friday'
-            break;
-        case 6:
-            day = 'Saturday'
-            break;
-        default:
-            break;
-    }
+    // switch (today.getDay()) {
+    //     case 0:
+    //         day = 'Sunday'
+    //         break;
+    //     case 1: 
+    //         day = 'Monday'
+    //         break;
+    //     case 2: 
+    //         day = 'Tuesday'
+    //         break;
+    //     case 3:
+    //         day = 'Wednesday'
+    //         break;
+    //     case 4:
+    //         day = 'Thursday'
+    //         break;
+    //     case 5:
+    //         day = 'Friday'
+    //         break;
+    //     case 6:
+    //         day = 'Saturday'
+    //         break;
+    //     default:
+    //         break;
+    // }
 
-
-
+    let day = date();
+    console.log(day)
     res.render('list', {kindOfDay: day, itemArr: items})
 })
 
